c4ae760ae4289f33bcd47be9ea5a634811a49903
hydrae-soft <torres.lf21@gmail.com>
2023-06-10 17:24:19 -0300

    add: deploy workflow 6

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { HashRouter } from "react-router-dom"
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
-      <App />
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
